2a438821773f792451f686323bfeaa69c17acf1c
Hristinchet0 <bansko.sport@gmail.com>
2023-09-25 23:44:04 +0300

    Add

---
+++ src/main/java/com/grturbo/grturbofullstackproject/GrTurboFullStackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.grturbo.grturbofullstackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrTurboFullStackProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrTurboFullStackProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grturbo/grturbofullstackproject/GrTurboFullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grturbo.grturbofullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrTurboFullStackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
