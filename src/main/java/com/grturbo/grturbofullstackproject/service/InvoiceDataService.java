77276a36bf0930d63df70236b13c76e7bcca279d
Hristinchet0 <bansko.sport@gmail.com>
2023-10-06 16:35:15 +0300

    Try to add invoice option in checkout, but not work. Add additional information in checkout.

---
+++ src/main/java/com/grturbo/grturbofullstackproject/service/InvoiceDataService.java
@@ -0,0 +1,20 @@
+//package com.grturbo.grturbofullstackproject.service;
+//
+//import com.grturbo.grturbofullstackproject.model.entity.InvoiceData;
+//import com.grturbo.grturbofullstackproject.repositority.InvoiceDataRepository;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class InvoiceDataService {
+//
+//    private final InvoiceDataRepository invoiceDataRepository;
+//
+//    public InvoiceDataService(InvoiceDataRepository invoiceDataRepository) {
+//        this.invoiceDataRepository = invoiceDataRepository;
+//    }
+//
+//    public InvoiceData saveInvoiceData(InvoiceData invoiceData) {
+//
+//        return invoiceDataRepository.save(invoiceData);
+//    }
+//}
