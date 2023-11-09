5204594e7a4fd29f1dd73b676b0227ae40dea6c0
Hristinchet0 <bansko.sport@gmail.com>
2023-11-09 10:47:16 +0200

    testing HomeController with integration tests

--- src/test/java/com/grturbo/grturbofullstackproject/web/HomeControllerTest.java
+++ src/test/java/com/grturbo/grturbofullstackproject/web/HomeControllerTest.java
@@ -1,35 +1,104 @@
 package com.grturbo.grturbofullstackproject.web;
 
+import com.grturbo.grturbofullstackproject.model.CustomUserDetail;
+import com.grturbo.grturbofullstackproject.model.entity.ShoppingCart;
+import com.grturbo.grturbofullstackproject.model.entity.User;
 import com.grturbo.grturbofullstackproject.service.ShoppingCartService;
-import com.grturbo.grturbofullstackproject.service.UserService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@ContextConfiguration(classes = {HomeController.class})
-@ExtendWith(SpringExtension.class)
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+import com.grturbo.grturbofullstackproject.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class HomeControllerTest {
+
     @Autowired
-    private HomeController homeController;
+    private MockMvc mockMvc;
+
+    private String baseUrl = "http://localhost";
 
     @MockBean
     private ShoppingCartService shoppingCartService;
 
     @MockBean
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @Test
-    void testAbout() throws Exception {
-        SecurityMockMvcRequestBuilders.FormLoginRequestBuilder requestBuilder = SecurityMockMvcRequestBuilders.formLogin();
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(homeController).build().perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
+    @Disabled("principal error")
+    void testHomeWithPrincipal() throws Exception {
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setEmail("test@example.com");
+        mockUser.setUsername("testUser");
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        CustomUserDetail userDetail = new CustomUserDetail(1L, "topsecret", "test@example.com", "Hristina", "Racheva", authorities);
+
+        ShoppingCart mockCart = new ShoppingCart();
+        mockCart.setId(1L);
+        mockCart.setCustomer(mockUser);
+        mockCart.setTotalItems(5);
+
+        when(userService.findByEmail("test@example.com")).thenReturn(Optional.of(mockUser));
+        when(shoppingCartService.findByUserId(1L)).thenReturn(mockCart);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/index")
+                        .principal(userDetail::getUsername))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("totalItems", 5));
     }
+
+    @Test
+    void testHomeControllerWithoutPrincipal() throws Exception {
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeDoesNotExist("totalItems"));
+
+    }
+
+    @Test
+    void testAboutPageShown() throws Exception {
+        mockMvc.perform(get("/about")).
+                andExpect(status().isOk()).
+                andExpect(view().name("about"));
+    }
+
+    @Test
+    void testContactPageShown() throws Exception {
+        mockMvc.perform(get("/contactus")).
+                andExpect(status().isOk()).
+                andExpect(view().name("contactus"));
+    }
+
+    @Test
+    void testFaqPageShown() throws Exception {
+        mockMvc.perform(get("/faq")).
+                andExpect(status().isOk()).
+                andExpect(view().name("faq"));
+    }
+
+
+
+
+
 }
 
